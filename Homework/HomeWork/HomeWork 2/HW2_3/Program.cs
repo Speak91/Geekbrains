57e1b429c8b52e127a21e00da775ec1f3659f41f
Speak91 <i-valentin@mail.ru>
2021-04-04 22:24:51 +0900

    Выполнено 3 задание 2 урока

---
+++ Homework/HomeWork/HomeWork 2/HW2_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи число и выясним четное оно или нет"); // Просим пользователя ввести число 
+            double number;
+            while (double.TryParse(Console.ReadLine(), out number) == false) // проверяем введено ли число если нет просим ввести повторно
+            {
+                Console.WriteLine("введено не число, введите число");
+            }
+            double Remainder = Math.IEEERemainder(number, 2); // Проверяем есть ли остаток
+            if (Remainder == 0) //если остатка нет значит оно четное о чем и сообщаем пользователю 
+            {
+                Console.WriteLine("Четное");
+            }
+            else { Console.WriteLine("Нечетное"); }
+
+            Console.ReadKey();
+        }
+    }
+}

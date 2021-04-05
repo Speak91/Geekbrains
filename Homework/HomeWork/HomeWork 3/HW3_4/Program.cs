505c2ad49b0537c5f7fe0e6ac173f535780d2e83
Speak91 <i-valentin@mail.ru>
2021-04-05 22:22:23 +0900

    Выполнено 4 задание 4 урока (морской бой)

---
+++ Homework/HomeWork/HomeWork 3/HW3_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HW3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Игра морской бой");
+            char[,] array = new char[10, 10]  {
+                {'O','X','O','O','O','O','O','O','X','O'},
+                {'O','O','O','O','O','X','X','O','X','O'},
+                {'O','O','O','O','O','O','O','O','X','O'},
+                {'O','X','X','X','X','O','O','O','O','O'},
+                {'O','O','O','O','O','O','O','O','O','O'},
+                {'O','O','O','O','X','O','O','O','O','O'},
+                {'O','X','X','O','O','O','O','O','O','X'},
+                {'O','O','O','O','O','O','O','O','O','O'},
+                {'O','O','O','X','O','X','X','X','O','O'},
+                {'O','O','O','X','O','O','O','O','O','O'}};
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}

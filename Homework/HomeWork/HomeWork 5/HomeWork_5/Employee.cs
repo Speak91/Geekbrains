ea8ae8b3f13a82f7e0de5bb4aea31de084b9e4e4
Speak91 <i-valentin@mail.ru>
2021-04-15 15:17:00 +0900

    Выполнено дз к 5 уроку

---
+++ Homework/HomeWork/HomeWork 5/HomeWork_5/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_5
+{
+    class Employee
+    {
+        string name;
+        string Posiition;
+        string email;
+        string PhoneNumber;
+        int Salary;
+        public int age { get; }
+        public Employee(string name, string position, string email, string PhoneNumber, int salary, int age)
+        {
+            this.name = name;
+            this.Posiition = position;
+            this.email = email;
+            this.PhoneNumber = PhoneNumber;
+            this.Salary = salary;
+            this.age = age;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name} Должность: {Posiition} email: {email} \nномер телефона: {PhoneNumber} Зарплата: {Salary} Возраст: {age}");
+        }
+    }
+}

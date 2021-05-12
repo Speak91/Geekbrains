9af1c1f3f58f95e1b41538aeb7a8a7b8491396af
Speak91 <i-valentin@mail.ru>
2021-05-12 22:52:18 +0900

    Выполнено дз к 8 уроку

---
+++ Homework/HomeWork/HomeWork_8/MyClassLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyClassLibrary
+{
+    public class Class1
+    {
+        static public void Hello (string name)
+        {
+            Console.WriteLine($"Привет {name}");
+        }
+    }
+}
